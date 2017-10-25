93af33d05b160341f0855b011b4eb511e69e993f
allisonl1014 <allisonl1014@users.noreply.github.com>
2017-10-24 21:58:10 -0500

    button

---
+++ js/jquery_exercise.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#blue").delay(500).fadeOut("slow").delay(500).fadeIn("slow");
+
+	$("#blue").dblclick(function() {
+		$("#husky").fadeIn("fast");
+	});
+
+
+
+});
